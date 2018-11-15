03818af9b532ef10997ba773eadc7a813a854d2b
下松祐也 <yuuya@kudamatsuyuuyanoMacBook-ea.local>
2018-11-15 23:17:01 +0900

    更新しました

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,12 +1,18 @@
 class MissionsController < ApplicationController
+
   def index
-    @missions = Mission.all
+    @missions = Mission.all.order(created_at: :desc)
   end
 
   def new
   end
 
   def create
+
+    @mission =Mission.new(content: params[:content])
+    @mission.save
+
+    redirect_to("/missions/index")
   end
 
 
